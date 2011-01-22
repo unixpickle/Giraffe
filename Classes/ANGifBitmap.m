0d0178f5afb178a15c4437a7808e0706d89188fd
Alex Nichol <alexnichol@comcast.net>
2011-01-22 10:06:12 -0500

    Fixed colour overflow issue

--- Classes/ANGifBitmap.m
+++ Classes/ANGifBitmap.m
@@ -8,6 +8,10 @@
 
 #import "ANGifBitmap.h"
 
+static UInt8 make3 (UInt8 m) {
+	if (m >= 4) m = 3;
+	return m;
+}
 
 @implementation ANGifBitmap
 
@@ -43,11 +47,12 @@
 			//NSLog(@"Done.");
 			// now we need to compress the pixel
 			UInt8 small = 0;
-			small |= pxlData[1] / 64;
-			small |= (int)round((float)pxlData[2] / 64.0f) << 2;
-			small |= (int)round((float)pxlData[3] / 64.0f) << 4;
-			small |= (int)round((float)pxlData[0] / 64.0f) << 6;
+			small |= make3((int)round((float)pxlData[1] / 64.0f));
+			small |= make3((int)round((float)pxlData[2] / 64.0f)) << 2;
+			small |= make3((int)round((float)pxlData[3] / 64.0f)) << 4;
+			small |= make3((int)round((float)pxlData[0] / 64.0f)) << 6;
 			if (small == 0) small = 128;
+			if (pxlData[0] < 10) small = 0;
 			[returnData appendBytes:&small length:1];
 			//NSLog(@"Size: %d", [returnData length]);
 			[pool drain];
